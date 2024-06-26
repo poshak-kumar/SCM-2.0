f393df96ea1564350649cc27186793fb7037195f
Poshak Kumar <yadavposhak029@gmail.com>
2024-06-27 00:58:40 +0530

    Building the Navbar

--- SCM-2.0/src/main/java/com/scm/SCM_20/controllers/PageController.java
+++ SCM-2.0/src/main/java/com/scm/SCM_20/controllers/PageController.java
@@ -9,19 +9,37 @@ public class PageController {
     @RequestMapping("/")
     public String home(){
         System.out.println("Home page handler.............");
-        return "home";
+        return new String("home");
     }
 
     @RequestMapping("/about")
     public String about(){
         System.out.println("About page handler.............");
-        return "about";
+        return new String("about");
     }
 
     @RequestMapping("/services")
     public String services(){
         System.out.println("Services page handler.............");
-        return "services";
+        return new String("services");
+    }
+
+    @RequestMapping("/contact")
+    public String contact(){
+        System.out.println("Contact page handler.............");
+        return new String("contact");
+    }
+
+    @RequestMapping("/sign-up")
+    public String signUp(){
+        System.out.println("Sing-up page handler.............");
+        return new String("register");
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        System.out.println("Login page handler.............");
+        return new String("login");
     }
 
 }
