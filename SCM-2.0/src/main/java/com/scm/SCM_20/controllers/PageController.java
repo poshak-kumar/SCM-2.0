4a8374da7042a86e1e3c998283676b2402394049
Poshak Kumar <yadavposhak029@gmail.com>
2024-06-26 12:05:17 +0530

    Project { Smart Contact Manager } first commit.

---
+++ SCM-2.0/src/main/java/com/scm/SCM_20/controllers/PageController.java
@@ -0,0 +1,15 @@
+package com.scm.SCM_20.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("Home page handler.............");
+        return "home";
+    }
+
+}
